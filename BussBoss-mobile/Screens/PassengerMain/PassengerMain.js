0af39b4c5e71561a1732c39706f8b5c5f69473a7
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-23 23:57:18 +0300

    Frontend: added heading question in passnegerMain screen

--- BussBoss-mobile/Screens/PassengerMain/PassengerMain.js
+++ BussBoss-mobile/Screens/PassengerMain/PassengerMain.js
@@ -8,8 +8,19 @@ export default function PassengerMain() {
       <Background />
       <Logo />
       <Greeting />
+      <Text style={styles.search_title}>Where are you heading?</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  search_title: {
+    left: 35,
+    top: 130,
+    position: "absolute",
+    zIndex: 2,
+    fontSize: 16,
+    color: "white",
+    fontWeight: "bold",
+  },
+});
