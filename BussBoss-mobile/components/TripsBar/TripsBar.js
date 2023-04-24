9db33a2489fbea1aea44de27b8bc13cb9be2998d
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-24 05:22:57 +0300

    Frontend: added tirps count to TripsBar comp

--- BussBoss-mobile/components/TripsBar/TripsBar.js
+++ BussBoss-mobile/components/TripsBar/TripsBar.js
@@ -1,6 +1,11 @@
 import { StyleSheet, View, Text } from "react-native";
 function TripsBar() {
-  return <View style={styles.trips_bar}></View>;
+  return (
+    <View style={styles.trips_bar}>
+      <Text style={styles.trips_title}>TOTAL TRIPS</Text>
+      <Text style={styles.trips_count}>102</Text>
+    </View>
+  );
 }
 export default TripsBar;
 
@@ -14,5 +19,19 @@ const styles = StyleSheet.create({
     position: "absolute",
     zIndex: 2,
     borderRadius: 15,
+  },
+  trips_title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "rgba(255, 255, 255, 0.5)",
+    top: 20,
+    left: 30,
+  },
+  trips_count: {
+    fontSize: 42,
+    fontWeight: "bold",
+    color: "white",
+    top: 20,
+    left: 40,
   },
 });
