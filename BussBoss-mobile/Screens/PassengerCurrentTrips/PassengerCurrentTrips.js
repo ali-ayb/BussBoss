d60d161f1d7cb9fe39d2d204a1f7ed9574689c23
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-24 08:51:57 +0300

    Frontend: added map_marker image

--- BussBoss-mobile/Screens/PassengerCurrentTrips/PassengerCurrentTrips.js
+++ BussBoss-mobile/Screens/PassengerCurrentTrips/PassengerCurrentTrips.js
@@ -1,24 +1,23 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import Background from "../../components/Background/Background";
 import Logo from "../../components/Logo/Logo";
-import Greeting from "../../components/Greeting/Greeting";
-import Search from "../../components/Search/Search";
-import TripsBar from "../../components/TripsBar/TripsBar";
-import DriverCard from "../../components/DriverCard/DriverCard";
+
 export default function PassengerMain() {
   return (
     <View style={{ backgroundColor: "#F6F1F1", flex: 1 }}>
       <Background />
       <Logo />
-      <Greeting />
-      <Text style={styles.search_title}>Where are you heading?</Text>
-      <Search />
-      <TripsBar />
-      <Text style={styles.choose_driver}>Choose your driver</Text>
-      <View style={{ flexDirection: "column", gap: 10 }}>
-        <DriverCard />
-        <DriverCard />
-      </View>
+      <Image
+        source={require("../../assets/map_marker.png")}
+        style={{
+          top: 120,
+          left: "40%",
+          width: 90,
+          height: 90,
+          zIndex: 2,
+          position: "absolute",
+        }}
+      />
     </View>
   );
 }
@@ -26,23 +25,5 @@ export default function PassengerMain() {
 const styles = StyleSheet.create({
   main: {
     backgroundColor: "#F6F1F1",
-  },
-  search_title: {
-    left: 35,
-    top: 130,
-    position: "absolute",
-    zIndex: 2,
-    fontSize: 16,
-    color: "white",
-    fontWeight: "bold",
-  },
-  choose_driver: {
-    left: 40,
-    top: 380,
-    position: "absolute",
-    zIndex: 2,
-    fontSize: 18,
-    color: "black",
-    fontWeight: "bold",
   },
 });
