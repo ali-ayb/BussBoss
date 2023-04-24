75f0ed1fe95cfb45598a7d9561f0e61feb3573d0
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-24 06:19:00 +0300

    Frontend: added card of the driver

--- BussBoss-mobile/components/DriverCard/DriverCard.js
+++ BussBoss-mobile/components/DriverCard/DriverCard.js
@@ -1,11 +1,16 @@
 import { StyleSheet, View, Text } from "react-native";
 
 function DriverCard() {
-  return <View style={{ backgroundColor: "#F6F1F1", flex: 1 }}></View>;
+  return <View style={styles.driver_card}></View>;
 }
 export default DriverCard;
 const styles = StyleSheet.create({
-  main: {
-    backgroundColor: "#F6F1F1",
+  driver_card: {
+    backgroundColor: "#FFF",
+    height: 150,
+    width: 335,
+    top: 70,
+    left: 35,
+    borderRadius: 15,
   },
 });
