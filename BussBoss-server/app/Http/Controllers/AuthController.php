7d2d3955128b6284680643dc260bf3ada09e6343
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-28 01:54:53 +0300

    Backend: added valdiation in register function

--- BussBoss-server/app/Http/Controllers/AuthController.php
+++ BussBoss-server/app/Http/Controllers/AuthController.php
@@ -46,11 +46,12 @@ class AuthController extends Controller
     public function register(Request $request)
     {
 
-        // $request->validate([
-        //     'name' => 'required|string|max:255',
-        //     'email' => 'required|string|email|max:255|unique:users',
-        //     'password' => 'required|string|min:6',
-        // ]);
+        $request->validate([
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
 
         $user = User::create([
             'first_name' => $request->first_name,
