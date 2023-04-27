62f64e70e2ebed5f27ae4c69c144603546e63cd5
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-28 00:40:05 +0300

    Backend: create RegisterController

--- BussBoss-server/routes/api.php
+++ BussBoss-server/routes/api.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::controller(AuthController::class)->group(function () {
+});
 Route::post('/register', [RegisterController::class, "register"]);
