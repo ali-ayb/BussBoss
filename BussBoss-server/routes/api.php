d3eb77643178ac6b60974cdb7caa4abe35f61dc9
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-28 00:47:37 +0300

    Backend: deleted RegisterController

--- BussBoss-server/routes/api.php
+++ BussBoss-server/routes/api.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(AuthController::class)->group(function () {
+    Route::post('/refresh', 'refresh');
 });
 Route::post('/register', [RegisterController::class, "register"]);
