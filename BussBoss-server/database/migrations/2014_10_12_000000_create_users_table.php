ab9df7d6c32909d53077aef012a9c56e28f62d12
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-27 03:27:17 +0300

    Backend: updated the users migration

--- BussBoss-server/database/migrations/2014_10_12_000000_create_users_table.php
+++ BussBoss-server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->unique();
             $table->string('password');
+            $table->enum('user_type', ['admin', 'passenger', 'driver'])->default('passenger');
+            $table->boolean('is_deleted')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
