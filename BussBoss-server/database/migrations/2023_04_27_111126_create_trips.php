c033de3e72e6c4c54f7d0d4811468c979f909c77
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-27 16:27:44 +0300

    Backend: added the trips table

---
+++ BussBoss-server/database/migrations/2023_04_27_111126_create_trips.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('departure_time');
+            $table->string('arrival_time');
+            $table->string('bus_number');
+            $table->string('source');
+            $table->string('destination');
+            $table->integer('seats_available');
+            $table->integer('min_passengers');
+            $table->boolean('is_deleted')->default(false);
+            $table->unsignedBigInteger('driver_id');
+            $table->foreign('driver_id')
+                ->references('id')
+                ->on('drivers_info')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
