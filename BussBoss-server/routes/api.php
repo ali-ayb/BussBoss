ddff200a174a4c8135aa384119b29aa19a2e247b
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-28 03:03:12 +0300

    Backend: added get passenger full name api route

--- BussBoss-server/routes/api.php
+++ BussBoss-server/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PassengerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, "register"]);
 Route::post('/login', [AuthController::class, "login"]);
+
+Route::get('/getPassengerFullName', [PassengerController::class, "getPassengerFullName"]);
