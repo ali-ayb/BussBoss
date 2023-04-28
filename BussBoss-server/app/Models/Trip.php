82969446ade56efb48fcc292a06f4f1b28265864
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-28 13:07:02 +0300

    Backend: added Trips model

---
+++ BussBoss-server/app/Models/Trip.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Trip extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'departure_time',
+        'arrival_time',
+        'bus_number',
+        'source',
+        'destination',
+        'seats_available',
+        'min_passengers',
+        'driver_id',
+    ];
+
+    public function Drivers_info(): BelongsTo
+    {
+        return $this->belongsTo(Drivers_info::class, 'driver_id');
+    }
+}
