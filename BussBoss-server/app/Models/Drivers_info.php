0640f1072a06dac8a1eda3d4184485a129b0c198
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-28 12:59:15 +0300

    Backend: added one to many relation in Drivers_info model 🔥

--- BussBoss-server/app/Models/Drivers_info.php
+++ BussBoss-server/app/Models/Drivers_info.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Drivers_info extends Model
 {
@@ -17,4 +18,9 @@ class Drivers_info extends Model
         'rating',
         'isapproved',
     ];
+
+    public function Trip(): HasMany
+    {
+        return $this->hasMany(Trip::class);
+    }
 }
