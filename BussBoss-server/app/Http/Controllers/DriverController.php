82b0d11280bec24d0bde552839aa3904eea68a2b
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-29 17:15:48 +0300

    Backend: created get drivers from destination api

--- BussBoss-server/app/Http/Controllers/DriverController.php
+++ BussBoss-server/app/Http/Controllers/DriverController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trip;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -9,11 +10,25 @@ class DriverController extends Controller
 {
     public function getAllDrivers()
     {
-        $users = User::join('drivers_info', 'users.id', '=', 'drivers_info.driver_id')
+        $drivers = User::join('drivers_info', 'users.id', '=', 'drivers_info.driver_id')
             ->get();
 
         return response()->json([
-            'drivers' => $users
+            'drivers' => $drivers
+        ]);
+    }
+
+    public function getDriversFromDestination(Request $request)
+    {
+        $destination = $request->destination;
+
+        $drivers = Trip::join('drivers_info', 'trips.driver_id', '=', 'drivers_info.driver_id')
+            ->join('users', 'trips.driver_id', '=', 'users.id')
+            ->select('trips.departure_time', 'trips.source', 'drivers_info.profile_image', 'drivers_info.rating', 'users.first_name', 'users.last_name')
+            ->get();
+
+        return response()->json([
+            'drivers' => $drivers
         ]);
     }
 }
