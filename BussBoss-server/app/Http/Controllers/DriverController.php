d2bbb9c8b29ca11b9a36a93bab5e3a01cf76afe7
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-29 16:12:23 +0300

    Backend: added get all drivers api

--- BussBoss-server/app/Http/Controllers/DriverController.php
+++ BussBoss-server/app/Http/Controllers/DriverController.php
@@ -2,8 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DriverController extends Controller
 {
+    public function getAllDrivers()
+    {
+        $users = User::join('drivers_info', 'users.id', '=', 'drivers_info.driver_id')
+            ->get();
+
+        return response()->json([
+            'drivers' => $users
+        ]);
+    }
 }
