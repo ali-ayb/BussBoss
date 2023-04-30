d66c038b1e84e939a08fe7d2e09f8d0249c59a8f
Ali-Ayb <aliayoub.me1@gmail.com>
2023-04-30 21:06:02 +0300

    Backend: added authorize driver  middleware🔥

--- BussBoss-server/routes/api.php
+++ BussBoss-server/routes/api.php
@@ -22,26 +22,36 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, "register"]);
 Route::post('/login', [AuthController::class, "login"]);
 
-Route::post('/reserve_trip', [ReservationController::class, "reserveTrip"]);
-Route::get('/get_passenger_current_reservations', [ReservationController::class, "getPassengerCurrentReservations"]);
 
 
-Route::get('/get_passenger_finished_trips', [TripController::class, "getPassengerFinishedTrips"]);
 
 
-Route::get('/get_all_Passengers', [PassengerController::class, "getAllPassengers"]);
-Route::get('/get_all_drivers', [DriverController::class, "getAllDrivers"]);
-Route::get('/get_drivers_from_destination', [DriverController::class, "getDriversFromDestination"]);
-Route::get('/get_trips_from_driver_id', [TripController::class, "getTripsFromDriverId"]);
-Route::get('/get_driver_total_trips', [DriverController::class, "getDriverTotalTrips"]);
-Route::get('/get_driver_total_earned', [DriverController::class, "getDriverTotalEarned"]);
+
+
+
+
 
 
 Route::group(['middleware' => 'bussboss_authenticate'], function () {
 
+    Route::post('/reserve_trip', [ReservationController::class, "reserveTrip"]);
     Route::get('/get_passenger_full_name', [PassengerController::class, "getPassengerFullName"]);
-    Route::get('/get_driver_full_name', [DriverController::class, "getDriverFullName"]);
-    Route::post('/add_trip', [TripController::class, "addTrip"]);
+    Route::get('/get_passenger_current_reservations', [ReservationController::class, "getPassengerCurrentReservations"]);
+    Route::get('/get_passenger_finished_trips', [TripController::class, "getPassengerFinishedTrips"]);
+    Route::get('/get_drivers_from_destination', [DriverController::class, "getDriversFromDestination"]);
+    Route::get('/get_trips_from_driver_id', [TripController::class, "getTripsFromDriverId"]);
+    Route::get('/get_Passenger_total_paid', [PassengerController::class, "getPassengerTotalPaid"]);
     Route::get('/get_passenger_total_trips', [PassengerController::class, "getPassengerTotalTrips"]);
-    Route::get('/get_total_paid', [PassengerController::class, "getTotalPaid"]);
+
+    Route::group(['middleware' => 'driver_authorize'], function () {
+        Route::get('/get_driver_total_trips', [DriverController::class, "getDriverTotalTrips"]);
+        Route::get('/get_driver_total_earned', [DriverController::class, "getDriverTotalEarned"]);
+        Route::get('/get_driver_full_name', [DriverController::class, "getDriverFullName"]);
+        Route::post('/add_trip', [TripController::class, "addTrip"]);
+    });
+
+    Route::group(['middleware' => 'admin_authorize'], function () {
+        Route::get('/get_all_Passengers', [PassengerController::class, "getAllPassengers"]);
+        Route::get('/get_all_drivers', [DriverController::class, "getAllDrivers"]);
+    });
 });
