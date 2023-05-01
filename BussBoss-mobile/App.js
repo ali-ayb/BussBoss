49e38da4f646b020eefcba99bc536f1ba2aced3d
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-01 23:32:27 +0300

    Frontend: added Navigation container to app.js

--- BussBoss-mobile/App.js
+++ BussBoss-mobile/App.js
@@ -1,8 +1,15 @@
 import { StyleSheet, View } from "react-native";
 import PassengerMain from "./Screens/PassengerMain/PassengerMain";
 import TabStack from "./navigation/TabStack";
+import OnboardingStack from "./navigation/OnboardingStack";
+import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
-  return <TabStack />;
+  return (
+    <NavigationContainer>
+      <TabStack />
+      {/* <OnboardingStack /> */}
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({});
