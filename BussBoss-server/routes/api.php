0ba0e097443e911713519109e082a3aba63888a0
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-01 04:11:56 +0300

    Backend: removed some spaces from the api file

--- BussBoss-server/routes/api.php
+++ BussBoss-server/routes/api.php
@@ -22,16 +22,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, "register"]);
 Route::post('/login', [AuthController::class, "login"]);
 
-
-
-
-
-
-
-
-
-
-
 Route::group(['middleware' => 'bussboss_authenticate'], function () {
 
     Route::post('/reserve_trip', [ReservationController::class, "reserveTrip"]);
