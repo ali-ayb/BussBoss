1a33018dd6ff8282fe5eb6ff8029623112aa23f0
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-01 23:32:43 +0300

    Frontend: updated onboarding stack

--- BussBoss-mobile/navigation/OnboardingStack.js
+++ BussBoss-mobile/navigation/OnboardingStack.js
@@ -1,22 +1,30 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import BussSchedule from "../Screens/BussSchedule/BussSchedule";
 import PassengerMain from "../Screens/PassengerMain/PassengerMain";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
-const OnboardingStack = () => {
-  // const PassengerStack = createStackNavigator();
-  // const DriverStack = createStackNavigator();
-  // const BussSchedule = createStackNavigator();
+const OnboardingStack = ({ navigation }) => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="PassengerMain" component={PassengerMain} />
-
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: true,
+        headerLeft: () => (
+          <Ionicons
+            name="arrow-back"
+            size={32}
+            color="black"
+            onPress={() => navigation.goBack()}
+          />
+        ),
+      }}>
       <Stack.Screen
         name="BussSchedule"
         component={BussSchedule}
-        header={false}
+        options={{ tabBarVisible: false }}
       />
+      <Stack.Screen name="PassengerMain" component={PassengerMain} />
     </Stack.Navigator>
   );
 };
