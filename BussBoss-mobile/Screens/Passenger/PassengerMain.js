e472509559b46d69c42abfdf6ad415f3d4085aa4
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-02 00:01:28 +0300

    Frontend: added passenger main file ❤️

---
+++ BussBoss-mobile/Screens/Passenger/PassengerMain.js
@@ -0,0 +1,58 @@
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import Background from "../../components/Background/Background";
+import Logo from "../../components/Logo/Logo";
+import Greeting from "../../components/Greeting/Greeting";
+import Search from "../../components/Search/Search";
+import TripsBar from "../../components/TripsBar/TripsBar";
+import DriverCard from "../../components/DriverCard/DriverCard";
+import { useNavigation } from "@react-navigation/native";
+
+export default function PassengerMain() {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.navigate("OnboardingStack");
+  };
+  return (
+    <View style={{ backgroundColor: "#F6F1F1", flex: 1 }}>
+      <Background />
+      <Logo />
+      <Greeting />
+      <Text style={styles.search_title}>Where are you heading?</Text>
+      <Search />
+      <TripsBar />
+      <Text style={styles.choose_driver}>Choose your driver</Text>
+      <View style={{ flexDirection: "column", gap: 10 }}>
+        <TouchableOpacity onPress={handlePress}>
+          <DriverCard />
+        </TouchableOpacity>
+
+        <DriverCard />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  main: {
+    backgroundColor: "#F6F1F1",
+  },
+  search_title: {
+    left: 35,
+    top: 130,
+    position: "absolute",
+    zIndex: 2,
+    fontSize: 16,
+    color: "white",
+    fontWeight: "bold",
+  },
+  choose_driver: {
+    left: 40,
+    top: 380,
+    position: "absolute",
+    zIndex: 2,
+    fontSize: 18,
+    color: "black",
+    fontWeight: "bold",
+  },
+});
