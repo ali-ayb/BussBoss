2d07453d3abd73de6a46f08f8dc9cec5fd2fb02d
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-02 23:23:09 +0300

    Frontend: added login page to app.js file

--- BussBoss-mobile/App.js
+++ BussBoss-mobile/App.js
@@ -2,15 +2,17 @@ import { StyleSheet, View } from "react-native";
 import TabStack from "./navigation/TabStack";
 import DriverTabStack from "./navigation/DriverTabStack";
 import DriverMain from "./Screens/Driver/DriverMain";
+import Login from "./Screens/Auth/Login";
 import OnboardingStack from "./navigation/OnboardingStack";
 import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
     <NavigationContainer>
       {/* <TabStack /> */}
-      <DriverTabStack />
+      {/* <DriverTabStack /> */}
       {/* <DriverMain /> */}
       {/* <OnboardingStack /> */}
+      <Login />
     </NavigationContainer>
   );
 }
