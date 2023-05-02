0c08cd3eaa0d2369f39a39a2828e68ae409dd579
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-02 23:40:30 +0300

    Frontend: added the text inputs and the login button

--- BussBoss-mobile/Screens/Auth/Login.js
+++ BussBoss-mobile/Screens/Auth/Login.js
@@ -1,48 +1,61 @@
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import Background from "../../components/Background/Background";
 import CenteredLogo from "../../components/CenteredLogo/CenteredLogo";
-import Greeting from "../../components/Greeting/Greeting";
-import Search from "../../components/Search/Search";
-import DriverTripsBar from "../../components/DriverTripBar/DriverTripBar";
-import DriverTripCard from "../../components/DriverTripCard/DriverTripCard";
-import { useNavigation } from "@react-navigation/native";
 
+function handlePress() {
+  alert("test");
+}
 export default function Login() {
   return (
     <View style={{ backgroundColor: "#F6F1F1", flex: 1 }}>
       <Background />
       <CenteredLogo />
-      {/* <TouchableOpacity style={styles.add_trip_btn} onPress={onPressLearnMore}>
-        <Text style={styles.add_trip_txt}>Add Trip</Text>
-      </TouchableOpacity> */}
+      <Text style={styles.welcome_title}>Welcome Back.</Text>
+      <View style={{ gap: 20 }}>
+        <TextInput style={styles.TextInput} placeholder="  Phone Number" />
+        <TextInput style={styles.TextInput} placeholder="  Password" />
+      </View>
+      <TouchableOpacity>
+        <View style={styles.Login_btn} onPress={handlePress}>
+          <Text style={{ fontSize: 24, color: "#FFF", top: 8, left: 125 }}>
+            Login
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  main: {
-    backgroundColor: "#F6F1F1",
-  },
-  add_trip_btn: {
-    backgroundColor: "#146C94",
-    borderRadius: 15,
-    padding: 10,
-    left: 230,
-    top: 45,
-    width: 130,
-  },
-  add_trip_txt: {
-    color: "#FFF",
-    fontSize: 18,
-    left: 22,
-  },
-  title: {
-    left: 40,
-    top: 390,
-    position: "absolute",
-    zIndex: 2,
-    fontSize: 24,
-    color: "black",
+  welcome_title: {
+    fontSize: 32,
     fontWeight: "bold",
+    justifyContent: "center",
+    alignContent: "center",
+    left: 110,
+  },
+  TextInput: {
+    backgroundColor: "#FFF",
+    padding: 10,
+    width: 300,
+    borderRadius: 15,
+    top: 70,
+    left: 50,
+    elevation: 10,
+  },
+  Login_btn: {
+    backgroundColor: "#146C94",
+    width: 300,
+    height: 50,
+    top: 130,
+    left: 50,
+    borderRadius: 15,
+    elevation: 5,
   },
 });
