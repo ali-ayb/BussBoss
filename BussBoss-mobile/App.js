6c3ff7a51672ccc2abd3a6ccb530673752ee3015
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-03 19:09:32 +0300

    Frontend: added provider to  app.js file

--- BussBoss-mobile/App.js
+++ BussBoss-mobile/App.js
@@ -6,15 +6,15 @@ import RegisterPassenger from "./Screens/Auth/RegisterPassenger";
 import Login from "./Screens/Auth/Login";
 import OnboardingStack from "./navigation/PassengerOnboardingStack";
 import { NavigationContainer } from "@react-navigation/native";
+import StackSwitcher from "./navigation/StackSwitcher";
+import { Provider } from "react-redux";
+
+import { store } from "./redux/store";
 export default function App() {
   return (
-    <NavigationContainer>
-      {/* <TabStack /> */}
-      <DriverMain />
-      {/* <OnboardingStack /> */}
-      {/* <Login /> */}
-      {/* <RegisterPassenger /> */}
-    </NavigationContainer>
+    <Provider store={store}>
+      <StackSwitcher />
+    </Provider>
   );
 }
 
