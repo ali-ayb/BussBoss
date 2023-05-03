bea6ee4f3e3f9a187c338a5fd927180ccac76e1f
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-03 21:32:12 +0300

    Backend: removed the extra space in getpassenger full name in the passenger controller

--- BussBoss-server/app/Http/Controllers/PassengerController.php
+++ BussBoss-server/app/Http/Controllers/PassengerController.php
@@ -27,8 +27,8 @@ class PassengerController extends Controller
         $first_name = $user->first_name;
         $last_name = $user->last_name;
         return response()->json([
-            'first_name ' => $first_name,
-            'last_name ' => $last_name,
+            'first_name' => $first_name,
+            'last_name' => $last_name,
         ]);
     }
 
