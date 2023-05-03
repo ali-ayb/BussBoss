ad6265164534e2058c8b9dfa3b33ffdcf1585b4c
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-03 21:24:20 +0300

    Frontend: created the main request hook

---
+++ BussBoss-mobile/hooks/request.js
@@ -0,0 +1,22 @@
+const UseHttp = async (url, method = "GET", body = "", header = {}) => {
+  let data;
+  const URL = "http://192.168.1.4:8000/api/";
+  try {
+    const Response =
+      (await fetch(URL + url, {
+        method: method,
+        headers: header,
+        body: !!body ? body : null,
+      })) || null;
+    data = await Response.json();
+    if (!Response.ok) {
+      throw new Error(data.message);
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  return data;
+};
+
+export default UseHttp;
