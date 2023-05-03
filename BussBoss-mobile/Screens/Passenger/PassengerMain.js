bd0eac503a2f57bee8beb4ae98156f9651cb771f
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-03 23:23:33 +0300

    Frontend: added post request in the passenger main page

--- BussBoss-mobile/Screens/Passenger/PassengerMain.js
+++ BussBoss-mobile/Screens/Passenger/PassengerMain.js
@@ -6,8 +6,31 @@ import Search from "../../components/Search/Search";
 import TripsBar from "../../components/TripsBar/TripsBar";
 import DriverCard from "../../components/DriverCard/DriverCard";
 import { useNavigation } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import UseHttp from "../../hooks/request";
 
 export default function PassengerMain() {
+  const [destination, setDestination] = useState("");
+  const formData = new FormData();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      formData.append("destination", destination);
+      const result = await UseHttp(
+        "get_drivers_from_destination",
+        "POST",
+        formData,
+        {
+          Authorization:
+            "Bearer " +
+            `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTkyLjE2OC4xLjQ6ODAwMC9hcGkvbG9naW4iLCJpYXQiOjE2ODMxNDM3NzIsImV4cCI6MTY4MzE0NzM3MiwibmJmIjoxNjgzMTQzNzcyLCJqdGkiOiIzRkw2TG9mNlU1Z0p5aVZOIiwic3ViIjoiNCIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.rhAN3q4ST2fFrZtfT4Qa0ylaqigMaAgVGwPdb2YV3SU`,
+        }
+      );
+      console.log(result);
+    };
+    fetchData();
+  }, [destination]);
+
   const navigation = useNavigation();
 
   const handlePress = () => {
@@ -19,7 +42,7 @@ export default function PassengerMain() {
       <Logo />
       <Greeting />
       <Text style={styles.search_title}>Where are you heading?</Text>
-      <Search />
+      <Search userInput={destination} setUserInput={setDestination} />
       <TripsBar />
       <Text style={styles.choose_driver}>Choose your driver</Text>
       <View style={{ flexDirection: "column", gap: 10 }}>
