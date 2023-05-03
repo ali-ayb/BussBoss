a059a63544975c487cec2cce11fee714f39832ed
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-03 21:26:41 +0300

    Frontend: updated the passenger onboarding stack

--- BussBoss-mobile/navigation/PassengerOnboardingStack.js
+++ BussBoss-mobile/navigation/PassengerOnboardingStack.js
@@ -2,31 +2,30 @@ import { createStackNavigator } from "@react-navigation/stack";
 import BussSchedule from "../Screens/Passenger/BussSchedule";
 import PassengerMain from "../Screens/Passenger/PassengerMain";
 import { Ionicons } from "@expo/vector-icons";
+import PassengerTabStack from "./PassengerTabStack";
 
 const Stack = createStackNavigator();
 
-const OnboardingStack = ({ navigation }) => {
+const PassengerOnboardingStack = ({ navigation }) => {
   return (
     <Stack.Navigator
-      screenOptions={{
-        headerShown: true,
-        headerLeft: () => (
-          <Ionicons
-            name="arrow-back"
-            size={32}
-            color="black"
-            onPress={() => navigation.goBack()}
-          />
-        ),
-      }}>
-      <Stack.Screen
-        name="BussSchedule"
-        component={BussSchedule}
-        options={{ tabBarVisible: false }}
-      />
-      <Stack.Screen name="PassengerMain" component={PassengerMain} />
+    // screenOptions={{
+    //   headerShown: true,
+    //   headerLeft: () => (
+    //     <Ionicons
+    //       name="arrow-back"
+    //       size={32}
+    //       color="black"
+    //       onPress={() => navigation.goBack()}
+    //     />
+    //   ),
+    // }}
+    >
+      <Stack.Screen name="Passenger" component={PassengerTabStack} />
+      <Stack.Screen name="BussSchedule" component={BussSchedule} />
+      {/* <Stack.Screen name="PassengerOnboardingStack" component={PassengerMain} /> */}
     </Stack.Navigator>
   );
 };
 
-export default OnboardingStack;
+export default PassengerOnboardingStack;
