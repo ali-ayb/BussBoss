0126cb75290ffc07e2cf27ed9bde15acf1872833
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-03 14:11:57 +0300

    Frontend: updated the app.js file

--- BussBoss-mobile/App.js
+++ BussBoss-mobile/App.js
@@ -1,18 +1,19 @@
 import { StyleSheet, View } from "react-native";
-import TabStack from "./navigation/TabStack";
-import DriverTabStack from "./navigation/DriverTabStack";
+import TabStack from "./navigation/PassengerTabStack";
 import DriverMain from "./Screens/Driver/DriverMain";
+import AllTrips from "./Screens/Driver/AllTrips";
+import RegisterPassenger from "./Screens/Auth/RegisterPassenger";
 import Login from "./Screens/Auth/Login";
-import OnboardingStack from "./navigation/OnboardingStack";
+import OnboardingStack from "./navigation/PassengerOnboardingStack";
 import { NavigationContainer } from "@react-navigation/native";
 export default function App() {
   return (
     <NavigationContainer>
       {/* <TabStack /> */}
-      {/* <DriverTabStack /> */}
-      {/* <DriverMain /> */}
+      <DriverMain />
       {/* <OnboardingStack /> */}
-      <Login />
+      {/* <Login /> */}
+      {/* <RegisterPassenger /> */}
     </NavigationContainer>
   );
 }
