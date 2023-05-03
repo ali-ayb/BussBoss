91afb03136e2f12a5e3342884949dfefc7652ffa
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-03 21:24:43 +0300

    Frontend: created the auth stack

---
+++ BussBoss-mobile/navigation/Authstack.js
@@ -0,0 +1,27 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import BussSchedule from "../Screens/Passenger/BussSchedule";
+import PassengerMain from "../Screens/Passenger/PassengerMain";
+import { Ionicons } from "@expo/vector-icons";
+import PassengerTabStack from "./PassengerTabStack";
+import Login from "../Screens/Auth/Login";
+import PreRegister from "../Screens/Auth/PreRegister";
+import RegisterDriver from "../Screens/Auth/RegisterDriver";
+import RegisterPassenger from "../Screens/Auth/RegisterPassenger";
+
+const Stack = createStackNavigator();
+
+const AuthStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login Screen" component={Login} />
+      <Stack.Screen name="Preregister Screen" component={PreRegister} />
+      <Stack.Screen name="RegisterDriver Screen" component={RegisterDriver} />
+      <Stack.Screen
+        name="RegisterPassenger Screen"
+        component={RegisterPassenger}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default AuthStack;
