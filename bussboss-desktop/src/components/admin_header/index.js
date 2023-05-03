c1a7fd9346d88dd2ccc915820af5bb2843af5356
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-03 13:56:10 +0300

    Frontend: added admin header component

---
+++ bussboss-desktop/src/components/admin_header/index.js
@@ -0,0 +1,12 @@
+const AdminHeader = () => {
+  return (
+    <div class="container">
+      <div class="navbar">
+        <div class="admin_name">admin name</div>
+        <button class="logout"> Logout</button>
+      </div>
+    </div>
+  );
+};
+
+export default AdminHeader;
