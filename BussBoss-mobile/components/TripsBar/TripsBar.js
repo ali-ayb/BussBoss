a8add1312ed386bb20cafa5fc9b940f34de3a8ec
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-04 01:14:04 +0300

    Frontend: added get request for tripBar component

--- BussBoss-mobile/components/TripsBar/TripsBar.js
+++ BussBoss-mobile/components/TripsBar/TripsBar.js
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import UseHttp from "../../hooks/request";
+
 function TripsBar() {
+  const [total_paid, setTotalPaid] = useState("");
+  const [total_trips, setTotalTrips] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await UseHttp("get_Passenger_total_paid", "GET", "", {
+        Authorization:
+          "Bearer " +
+          `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTkyLjE2OC4xLjQ6ODAwMC9hcGkvbG9naW4iLCJpYXQiOjE2ODMxNTEyMjIsImV4cCI6MTY4MzE1NDgyMiwibmJmIjoxNjgzMTUxMjIyLCJqdGkiOiJCSU9uaWtqODkzSnVsWDk0Iiwic3ViIjoiNCIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.0YURYcYxZtJMsxK_CeNrOq0Rm8TWit74mQALbAucisM`,
+      });
+      setTotalPaid(result.total_paid);
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await UseHttp("get_passenger_total_trips", "GET", "", {
+        Authorization:
+          "Bearer " +
+          `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTkyLjE2OC4xLjQ6ODAwMC9hcGkvbG9naW4iLCJpYXQiOjE2ODMxNTEyMjIsImV4cCI6MTY4MzE1NDgyMiwibmJmIjoxNjgzMTUxMjIyLCJqdGkiOiJCSU9uaWtqODkzSnVsWDk0Iiwic3ViIjoiNCIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.0YURYcYxZtJMsxK_CeNrOq0Rm8TWit74mQALbAucisM`,
+      });
+      setTotalTrips(result.total_trips);
+    };
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.trips_bar}>
       <Text style={styles.trips_title}>TOTAL TRIPS</Text>
-      <Text style={styles.trips_count}>102</Text>
+      <Text style={styles.trips_count}>{total_trips}</Text>
       <Text style={styles.paid_title}>TOTAL PAID</Text>
-      <Text style={styles.paid_number}>52$</Text>
+      <Text style={styles.paid_number}>{total_paid}$</Text>
     </View>
   );
 }
@@ -34,7 +64,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "white",
     top: 20,
-    left: 40,
+    left: 65,
   },
   paid_title: {
     fontSize: 16,
@@ -49,6 +79,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: "white",
     top: -50,
-    left: 230,
+    left: 240,
   },
 });
