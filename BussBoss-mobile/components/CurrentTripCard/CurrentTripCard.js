31886eebd79d97aee8daf674725aeec17f9b631d
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-05 00:56:27 +0300

    Frontend: added props to Current trip card component

--- BussBoss-mobile/components/CurrentTripCard/CurrentTripCard.js
+++ BussBoss-mobile/components/CurrentTripCard/CurrentTripCard.js
@@ -1,8 +1,16 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect } from "react";
 import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
+
 function onPressLearnMore() {
   alert("test");
 }
-function CurrentTripCard() {
+function CurrentTripCard({ item }) {
+  const destination = item.destination;
+  const first_name = item.first_name;
+  const last_name = item.last_name;
+  const departure_time = new Date(item.departure_time);
+  const arrival_time = new Date(item.arrival_time);
   return (
     <View style={styles.CurrentTripCard}>
       <Image
@@ -13,16 +21,21 @@ function CurrentTripCard() {
         style={styles.pin_icon}
         source={require("../../assets/pin_icon.png")}
       />
-      <Text style={styles.trip_time}>10:00 {"<-->"} 10:30</Text>
-      <Text style={styles.trip_position}>To Position </Text>
+      <Text style={styles.trip_time}>
+        {departure_time.getHours()}:{departure_time.getMinutes()} {"<-->"}
+        {arrival_time.getHours()}:{arrival_time.getMinutes()}
+      </Text>
+      <Text style={styles.trip_position}>{destination} </Text>
 
-      <Text style={styles.driver_name}>Driver: Driver Name</Text>
+      <Text style={styles.driver_name}>
+        Driver: {first_name} {last_name}
+      </Text>
 
       <TouchableOpacity style={styles.finish_btn} onPress={onPressLearnMore}>
         <Text style={styles.finish_btn_text}>Finish Trip</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.track_btn} onPress={onPressLearnMore}>
-        <Text style={styles.track_btn_text}>Finish Trip</Text>
+        <Text style={styles.track_btn_text}>Track Bus</Text>
       </TouchableOpacity>
     </View>
   );
@@ -33,7 +46,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFF",
     height: 110,
     width: 335,
-    top: 85,
+    top: 20,
     left: "10%",
     borderRadius: 15,
     elevation: 20,
@@ -51,17 +64,20 @@ const styles = StyleSheet.create({
   trip_time: {
     top: -50,
     left: 60,
-    fontSize: 14,
+    fontWeight: "bold",
+    fontSize: 16,
   },
   trip_position: {
-    top: -25,
+    top: -30,
     left: 60,
-    fontSize: 14,
+    fontSize: 16,
+    fontWeight: "bold",
   },
   driver_name: {
-    top: -85,
+    top: -90,
     left: 200,
-    fontSize: 14,
+    fontSize: 16,
+    fontWeight: "bold",
   },
   track_btn: {
     borderRadius: 15,
@@ -71,7 +87,7 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     padding: 5,
     left: "73%",
-    top: "-55%",
+    top: "-64%",
     width: 80,
   },
   finish_btn_text: {
@@ -84,7 +100,7 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     padding: 5,
     left: "47%",
-    top: "-80%",
+    top: "-89%",
     width: 80,
   },
   track_btn_text: {
