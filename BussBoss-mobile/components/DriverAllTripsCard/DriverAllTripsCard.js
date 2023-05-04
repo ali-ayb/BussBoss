534611992766e2cb80b0b1ce1ada1cb9be2748ae
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-05 00:56:58 +0300

    Frontend: created driver all trips card component

---
+++ BussBoss-mobile/components/DriverAllTripsCard/DriverAllTripsCard.js
@@ -0,0 +1,92 @@
+import { StyleSheet, View, Image, Text, TouchableOpacity } from "react-native";
+import { Rating } from "react-native-ratings";
+function onPressLearnMore() {
+  alert("test");
+}
+function AllTripsCard() {
+  return (
+    <View style={styles.AllTripsCard}>
+      {/* <Text style={styles.driver_name}>Driver: Driver Name</Text> */}
+      <Text style={styles.driver_name}>Trip: Trip Number</Text>
+      <Image
+        style={styles.calender_icon}
+        source={require("../../assets/calender.png")}
+      />
+      <Image
+        style={styles.clock_icon}
+        source={require("../../assets/clock_icon.png")}
+      />
+      <Image
+        style={styles.pin_icon}
+        source={require("../../assets/pin_icon.png")}
+      />
+      <Text style={styles.trip_time}>10:00 {"<-->"} 10:30</Text>
+      <Text style={styles.trip_position}>
+        From Position {"<-->"} To Position{" "}
+      </Text>
+      <Text style={styles.date}>20/4/2023</Text>
+      <Text style={styles.rating}>Average Rating</Text>
+      <Rating
+        type="custom"
+        ratingColor="#146C94"
+        ratingCount={5}
+        imageSize={30}
+        ratingBackgroundColor="#D9D9D9"
+        tintColor="#FFF"
+        readonly="true"
+        onFinishRating={this.ratingCompleted}
+        style={{ paddingVertical: 10, top: -101, left: 40 }}
+      />
+    </View>
+  );
+}
+export default AllTripsCard;
+const styles = StyleSheet.create({
+  AllTripsCard: {
+    backgroundColor: "#FFF",
+    height: 140,
+    width: 335,
+    top: 0,
+    left: "10%",
+    borderRadius: 15,
+    elevation: 20,
+  },
+  rating: {
+    fontSize: 16,
+    top: -67,
+    left: 25,
+  },
+  calender_icon: { top: 17, left: 190 },
+  clock_icon: {
+    top: -10,
+    left: 25,
+  },
+  pin_icon: {
+    height: 50,
+    width: 30,
+    top: -15,
+    left: 20,
+  },
+  trip_time: {
+    top: -80,
+    left: 60,
+    fontSize: 14,
+  },
+  trip_position: {
+    top: -67,
+    left: 60,
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  driver_name: {
+    top: 10,
+    left: 25,
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  date: {
+    top: -115,
+    left: 230,
+    fontSize: 14,
+  },
+});
