e574db6293bb063a3a5d490b1e942a0ae49616fc
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-05 01:01:22 +0300

    Frontend: updated the user type stack so it became  more dynamic

--- BussBoss-mobile/navigation/UserTypeSwitcher.js
+++ BussBoss-mobile/navigation/UserTypeSwitcher.js
@@ -3,17 +3,38 @@ import { createStackNavigator } from "@react-navigation/stack";
 import DriverTabStack from "./DriverTabStack";
 import PassengerTabStack from "./PassengerTabStack";
 import PassengerOnboardingStack from "./PassengerOnboardingStack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import DriverOnboardingStack from "./DriverOnboardingStack";
 
 const UserTypeSwitcher = () => {
-  const isLoggedIn = true;
+  const [userType, setUserType] = useState(null);
+
+  useEffect(() => {
+    const retrieveUserType = async () => {
+      try {
+        const value = await AsyncStorage.getItem("user_type");
+        if (value !== null) {
+          setUserType(value);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    retrieveUserType();
+  }, []);
+
   const stack = createStackNavigator();
-  const user = { type: "passenger" };
+  const user = { type: userType };
+
   return (
     <stack.Navigator screenOptions={{ headerShown: false }}>
       <stack.Screen
-        name="User Screeen"
+        name="User Screen"
         component={
-          user.type == "passenger" ? PassengerOnboardingStack : DriverTabStack
+          user.type === "passenger"
+            ? PassengerOnboardingStack
+            : DriverOnboardingStack
         }
       />
     </stack.Navigator>
