d148499ce5144d11b90b19b90d0592d5f0506cb2
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-07 15:48:59 +0300

    Backend: added driver_id in the select statement for the get driver form destination api

--- BussBoss-server/app/Http/Controllers/DriverController.php
+++ BussBoss-server/app/Http/Controllers/DriverController.php
@@ -68,7 +68,7 @@ class DriverController extends Controller
         $drivers = Trip::where('destination', 'LIKE', $destination . '%')
             ->join('drivers_info', 'trips.driver_id', '=', 'drivers_info.driver_id')
             ->join('users', 'trips.driver_id', '=', 'users.id')
-            ->select('trips.departure_time', 'trips.source', 'drivers_info.profile_image', 'drivers_info.rating', 'users.first_name', 'users.last_name')
+            ->select('drivers_info.driver_id', 'trips.departure_time', 'trips.source', 'drivers_info.profile_image', 'drivers_info.rating', 'users.first_name', 'users.last_name')
             ->get();
 
         return response()->json([
