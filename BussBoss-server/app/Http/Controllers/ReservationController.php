0377d13b7eadc2fe90bd1e549391a9a7882a3010
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-07 22:45:53 +0300

    Backend: created addRating api

--- BussBoss-server/app/Http/Controllers/ReservationController.php
+++ BussBoss-server/app/Http/Controllers/ReservationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Drivers_info;
+use App\Models\DriverRating;
 use App\Models\Reservation;
 use App\Models\Trip;
 use Illuminate\Http\Request;
@@ -46,6 +48,30 @@ class ReservationController extends Controller
         ]);
     }
 
+    public function addRating(Request $request)
+    {
+        $rating = $request->rating;
+        $driver_id = $request->driver_id;
+
+        $reservation = DriverRating::create(
+            [
+                'driver_id' => $driver_id,
+                'rating' => $rating
+            ]
+        );
+
+        $ratings = DriverRating::where('driver_id', $driver_id)->get();
+
+        $average_rating = $ratings->avg('rating');
+
+        $action = Drivers_info::where('driver_id', $driver_id)
+            ->update(['rating' => $average_rating]);
+
+        return response()->json([
+            "result" => $action,
+        ]);
+    }
+
     public function cancelReservation(Request $request)
     {
         $reservation_id = $request->reservation_id;
@@ -69,7 +95,7 @@ class ReservationController extends Controller
             ->join('users', 'trips.driver_id', '=', 'users.id')
             ->where('reservations.passenger_id', $passenger_id)
             ->where('reservations.status', 'ongoing')
-            ->select('reservations.*', 'trips.source', 'trips.destination', 'trips.departure_time', 'trips.arrival_time', 'trips.bus_number', 'users.first_name', 'users.last_name')
+            ->select('reservations.*', 'trips.driver_id', 'trips.source', 'trips.destination', 'trips.departure_time', 'trips.arrival_time', 'trips.bus_number', 'users.first_name', 'users.last_name')
             ->get();
 
         return response()->json([
