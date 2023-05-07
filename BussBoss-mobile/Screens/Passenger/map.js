b43c93e989a7f5d4672fc8042cc55293a93adc6c
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-07 23:49:45 +0300

    Frontend: added arrival duration in the map screen

--- BussBoss-mobile/Screens/Passenger/map.js
+++ BussBoss-mobile/Screens/Passenger/map.js
@@ -1,13 +1,16 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import MapView from "react-native-maps";
 import { Marker } from "react-native-maps";
-import { Platform, StatusBar, StyleSheet } from "react-native";
+import { Platform, StatusBar, StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
 // import * as geoLocation from "expo-location";
 import MapViewDirections from "react-native-maps-directions";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function BussSchedule() {
+  const [duration, setDuration] = useState(null);
+  const [modified_duration, setModifiedDuration] = useState(null);
+
   const [myLocation, setMyLocation] = useState({
     latitude: 33.450736,
     longitude: 35.396315,
@@ -17,8 +20,20 @@ export default function BussSchedule() {
     longitude: 35.366315,
   });
 
+  const onReady = (result) => {
+    setDuration(result.duration);
+  };
+  console.log(duration);
+
+  useEffect(() => {
+    if (duration !== null && modified_duration === null) {
+      const modDur = duration.toString().slice(0, 4);
+      setModifiedDuration(modDur);
+    }
+  }, [duration, modified_duration]);
+
   return (
-    <SafeAreaView style={styles.mainView}>
+    <View style={styles.mainView}>
       <MapView
         style={styles.mapView}
         initialRegion={{
@@ -36,31 +51,43 @@ export default function BussSchedule() {
           apikey={Constants.manifest.extra.googleApiKey}
           strokeWidth={3}
           strokeColor={"#1B2126"}
+          onReady={onReady}
         />
 
         <Marker coordinate={userLocation} title={"Picker Location"} />
       </MapView>
-    </SafeAreaView>
+      <View style={styles.arrival_time}>
+        <Text
+          style={{ fontWeight: "bold", color: "#FFF", left: 45, fontSize: 17 }}>
+          Arrives In: {modified_duration} minutes
+        </Text>
+      </View>
+    </View>
   );
 }
-// const styles = StyleSheet.create({
-//   mainView: {
-//     backgroundColor: "#F6F1F1",
-//     marginBottom: 80,
-//   },
-// });
-
 const styles = StyleSheet.create({
   mainView: {
-    backgroundColor: "blue",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    backgroundColor: "#146C94",
+    // paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     flex: 1,
-    alignItems: "center",
-    height: "100%",
+    // alignItems: "center",
+    // height: "100%",
   },
   mapView: {
     flex: 1,
     height: "100%",
-    width: "100%",
+    // width: "100%",
+  },
+  arrival_time: {
+    height: 50,
+    width: "60%",
+    bottom: 0,
+    position: "absolute",
+    bottom: 100,
+    left: 85,
+    opacity: 0.9,
+    backgroundColor: "#146C94",
+    justifyContent: "center",
+    borderRadius: 15,
   },
 });
