b2562a66040f506dde6b911b3a00e067721ce75b
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-09 08:51:33 +0300

    Frontend: changed the api URL

--- bussboss-desktop/src/hooks/http-hook.js
+++ bussboss-desktop/src/hooks/http-hook.js
@@ -1,23 +1,22 @@
-
-const UseHttp =  async (url, method = "GET", body = "", header = {}) => {
-    let data;
-    const URL = "http://127.0.0.1:8000/api/v1/";
-    try {
-        const Response = await fetch(URL+url,{
-            method : method,
-            headers: header,
-            body: !!body ? body : null  
-        }) || null;
-        data = await Response.json();
-        if (!Response.ok){
-          throw new Error(data.message)
-        }
-    } catch (err) {
-
-      console.log(err.message);
+const UseHttp = async (url, method = "GET", body = "", header = {}) => {
+  let data;
+  const URL = "http://192.168.1.4:8000/api/";
+  try {
+    const Response =
+      (await fetch(URL + url, {
+        method: method,
+        headers: header,
+        body: !!body ? body : null,
+      })) || null;
+    data = await Response.json();
+    if (!Response.ok) {
+      throw new Error(data.message);
     }
-    
-    return data;
-  };
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  return data;
+};
 
 export default UseHttp;
