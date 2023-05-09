21a96429410d676ee1fe08c5cc66bf97eeb694cd
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-09 08:47:55 +0300

    Frontend: added drivers route path

--- bussboss-desktop/src/App.js
+++ bussboss-desktop/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route, Router } from "react-router-dom";
 import HomePage from "./pages/HomePage/index";
+import DriversTable from "./pages/Drivers/Drivers";
 import Login from "./components/login/login";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/admin_panel" element={<HomePage />} />
+      <Route path="/admin_panel/drivers" element={<DriversTable />} />
     </Routes>
   );
 }
