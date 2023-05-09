d3b01a5fe17c7e5ae2e7c3e5510d8c772087c10b
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-09 08:48:27 +0300

    Frontend: updated tablerows file

--- bussboss-desktop/src/components/admin/tablerows.js
+++ bussboss-desktop/src/components/admin/tablerows.js
@@ -1,12 +1,16 @@
-const Tr = (props)=>{
-  return(
+const Tr = (props) => {
+  console.log(props);
+  return (
     <tr>
-            <td>{props.data.id}</td>
-            <td>{props.data.username}</td>
-            <td>{props.data.first_name}</td>
-            <td>{props.data.last_name}</td>
-            <td>{props.data.is_deleted}</td>
+      <td>{props.data.first_name}</td>
+      <td>{props.data.last_name}</td>
+      <td>{props.data.email}</td>
+      <td>{props.data.total_trips}</td>
+      <td>{props.data.total_earned}</td>
+      <td>
+        <div className="delete_btn">Delete</div>
+      </td>
     </tr>
-  )
-}
-export default Tr
+  );
+};
+export default Tr;
