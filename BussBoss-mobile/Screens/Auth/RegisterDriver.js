99d640716b0c6c0c4b0e6a9ad91140d295a695c5
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-09 08:56:32 +0300

    Frontend: updated the register page

--- BussBoss-mobile/Screens/Auth/RegisterDriver.js
+++ BussBoss-mobile/Screens/Auth/RegisterDriver.js
@@ -4,61 +4,173 @@ import {
   Text,
   TouchableOpacity,
   TextInput,
+  Image,
+  ScrollView,
 } from "react-native";
 import RegisterLogo from "../../components/RegisterLogo/RegisterLogo";
-import { useNavigation } from "@react-navigation/native";
-import { RadioButton } from "react-native-paper";
 import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import { Feather } from "@expo/vector-icons";
+import * as FileSystem from "expo-file-system";
+import UseHttp from "../../hooks/request";
+import axios from "axios";
 
-function login() {
-  alert("register");
-}
 export default function RegisterDriver() {
-  const [value, setValue] = useState("first");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [first_name, setfirstName] = useState("Ali2");
+  const [last_name, setLastName] = useState("Ayoub");
+  const [email, setEmail] = useState("Ali2@gmail.com");
+  const [phone, setPhone] = useState("76102030");
+  const [password, setPassword] = useState("code123");
+  const [license_number, setLicenseNumber] = useState("F515F");
+  const [profile, setProfile] = useState("");
+
+  const formData = new FormData();
+
+  const handleRegister = async () => {
+    const formData = new FormData();
+    formData.append("first_name", first_name);
+    formData.append("last_name", last_name);
+    formData.append("email", email);
+    formData.append("phone", phone);
+    formData.append("password", password);
+    formData.append("license_number", license_number);
+    formData.append("user_type", "driver");
+    formData.append("profile_image", profile);
+    formData.append("rating", 5);
+
+    try {
+      const response = await axios.post(
+        "http://192.168.1.4:8000/api/register",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+            "Access-Control-Allow-Credentials": true,
+          },
+        }
+      );
+
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleChoosePhoto = async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (status !== "granted") {
+      alert("Sorry, Camera roll permissions needed to make this work!");
+      return;
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      const cacheDirectory = FileSystem.cacheDirectory;
+      const fileName = result.assets[0].uri.split("/").pop();
+      const filePath = `${cacheDirectory}${fileName}`;
+      try {
+        await FileSystem.copyAsync({
+          from: result.assets[0].uri,
+          to: filePath,
+        });
+
+        setProfile({
+          uri: filePath,
+          type: "image/jpeg, image/png",
+          name: fileName,
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   return (
-    <View style={{ backgroundColor: "#F6F1F1", flex: 1 }}>
+    <ScrollView style={{ backgroundColor: "#F6F1F1", flex: 1 }}>
       <View style={{ top: -60 }}>
         <RegisterLogo />
       </View>
 
-      <View style={{ gap: 20 }}>
-        <TextInput style={styles.first_name} placeholder="  First Name" />
-        <TextInput style={styles.last_name} placeholder="  Last Name" />
-        <TextInput style={styles.TextInput} placeholder="  Phone Number" />
-        <TextInput style={styles.TextInput} placeholder="  Password" />
-        <TextInput style={styles.TextInput} placeholder="  Birth Date" />
-        <TextInput style={styles.TextInput} placeholder="  Licence Number" />
-        <TextInput style={styles.TextInput} placeholder="  Image" />
-        <RadioButton.Group onValueChange={setValue} value={value}>
-          <View
-            style={{
-              flexDirection: "column",
-              alignItems: "center",
-              top: 160,
-              left: -100,
-            }}>
-            <RadioButton value="first" />
-            <Text>Male</Text>
+      <View style={{ gap: 20, marginBottom: 200 }}>
+        <TextInput
+          style={styles.first_name}
+          placeholder="  First Name"
+          onChangeText={(e) => {
+            setfirstName(e);
+          }}
+        />
+        <TextInput
+          style={styles.last_name}
+          placeholder="  Last Name"
+          onChangeText={(e) => {
+            setLastName(e);
+          }}
+        />
+        <TextInput
+          style={styles.Email}
+          placeholder="  Email"
+          onChangeText={(e) => {
+            setEmail(e);
+          }}
+        />
+        <TextInput
+          style={styles.Password}
+          placeholder="  Password"
+          onChangeText={(e) => {
+            setPassword(e);
+          }}
+        />
+        {/* <TextInput
+          style={styles.birth}
+          placeholder="  Birth Date"
+          onChangeText={(e) => {
+            setEmail(e);
+          }}
+        /> */}
+        <TextInput
+          style={styles.licence}
+          placeholder="  Licence Number"
+          onChangeText={(e) => {
+            setLicenseNumber(e);
+          }}
+        />
+        <Image
+          source={{ uri: selectedImage }}
+          style={{
+            width: 100,
+            height: 100,
+            left: 250,
+            top: 160,
+            borderRadius: 15,
+          }}
+        />
+        <TouchableOpacity onPress={handleChoosePhoto}>
+          <View style={styles.imageUpload}>
+            <View>
+              <Feather name="image" size={30} color={"blue"} />
+            </View>
+            <View>
+              <Text>Choose Photo</Text>
+            </View>
           </View>
-          <View
-            style={{
-              flexDirection: "column",
-              alignItems: "center",
-              top: 110,
-              left: 100,
-            }}>
-            <RadioButton value="second" />
-            <Text>Female</Text>
-          </View>
-        </RadioButton.Group>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.register_btn} onPress={handleRegister}>
+          <Text style={{ fontSize: 24, color: "#FFF", top: 8, left: 35 }}>
+            Register
+          </Text>
+        </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.register_btn} onPress={login}>
-        <Text style={{ fontSize: 24, color: "#FFF", top: 8, left: 35 }}>
-          Register
-        </Text>
-      </TouchableOpacity>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -81,12 +193,39 @@ const styles = StyleSheet.create({
     left: 220,
     elevation: 10,
   },
-  TextInput: {
+  Email: {
     backgroundColor: "#FFF",
     padding: 10,
     width: 320,
     borderRadius: 15,
-    top: 165,
+    top: 160,
+    left: 50,
+    elevation: 10,
+  },
+  Password: {
+    backgroundColor: "#FFF",
+    padding: 10,
+    width: 320,
+    borderRadius: 15,
+    top: 160,
+    left: 50,
+    elevation: 10,
+  },
+  birth: {
+    backgroundColor: "#FFF",
+    padding: 10,
+    width: 320,
+    borderRadius: 15,
+    top: 160,
+    left: 50,
+    elevation: 10,
+  },
+  licence: {
+    backgroundColor: "#FFF",
+    padding: 10,
+    width: 320,
+    borderRadius: 15,
+    top: 160,
     left: 50,
     elevation: 10,
   },
@@ -94,9 +233,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#146C94",
     width: 150,
     height: 50,
-    top: 120,
+    top: 100,
     left: 135,
     borderRadius: 15,
     elevation: 5,
+  },
+  imageUpload: {
+    top: 60,
+    left: 80,
   },
 });
