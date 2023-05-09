930211c4a21553bf8361b83e9b9649b9221e84ab
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-09 08:48:56 +0300

    Frontend: added navigaation to the admin panel

--- bussboss-desktop/src/components/admin_header/index.js
+++ bussboss-desktop/src/components/admin_header/index.js
@@ -2,7 +2,7 @@ const AdminHeader = () => {
   return (
     <div class="container">
       <div class="navbar">
-        <div class="admin_name">admin name</div>
+        <div class="admin_name">Hello admin</div>
         <button class="logout"> Logout</button>
       </div>
     </div>
