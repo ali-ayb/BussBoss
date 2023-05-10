7da7e88ea4bae23cff547102853ca290052b5487
Ali-Ayb <aliayoub.me1@gmail.com>
2023-05-10 16:32:38 +0300

    Frontend: added logut to the admin panel

--- bussboss-desktop/src/components/admin_header/index.js
+++ bussboss-desktop/src/components/admin_header/index.js
@@ -1,9 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import UseHttp from "../../hooks/http-hook";
+
 const AdminHeader = () => {
+  const navigate = useNavigate();
+
+  const handleLogoutClick = async () => {
+    const data = await UseHttp("logout", "POST", "", {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    });
+    console.log(data);
+    if (data.status === "success") {
+      navigate("/");
+    }
+  };
+
   return (
     <div class="container">
       <div class="navbar">
         <div class="admin_name">Hello admin</div>
-        <button class="logout"> Logout</button>
+        <button class="logout" onClick={handleLogoutClick}>
+          Logout
+        </button>
       </div>
     </div>
   );
